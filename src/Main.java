90e134f75cb3c2469e7e54b30ebe77b679946300
Fedenistov Dmitriy Vyacheslavovich <fdmitriy@inbox.ru>
2025-06-11 00:07:55 +0300

    Домашнее задание сделано и исправлено. Операторы if, else, switch, case, default ...

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Задача 1");
-        boolean clientOS = true;
+        boolean clientOS = false;
         if (clientOS) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         } else {
@@ -13,18 +13,14 @@ public class Main {
         // Task 2
         System.out.println("Задача 2");
         int clientDeviceYear = 2014;
-        if (clientOS) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            }
+        if (clientOS && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else if (clientOS && !(clientDeviceYear < 2015)) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (!clientOS && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
         } else {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
         System.out.println();
 
@@ -42,15 +38,9 @@ public class Main {
         System.out.println("Задача 4");
         int deliveryDistance = 95;
         int deliveryTime = 1;
-        if (deliveryDistance > 20) {
-            deliveryTime++;
-            if (deliveryDistance > 60) {
-                deliveryTime++;
-                if (deliveryDistance > 100) {
-                    deliveryTime = 0;
-                }
-            }
-        }
+        if (deliveryDistance > 20) { deliveryTime++; }
+        if (deliveryDistance > 60) { deliveryTime++; }
+        if (deliveryDistance > 100) { deliveryTime = 0; }
         if (deliveryTime != 0 ) {
             System.out.printf("Потребуется дней: %d.%n", deliveryTime);
         } else {
@@ -60,8 +50,8 @@ public class Main {
 
         // Task 5
         System.out.println("Задача 5");
-        int month = 12;
-        switch (month) {
+        int monthNumber = 12;
+        switch (monthNumber) {
             case 12, 1, 2 -> System.out.println("Зима.");
             case 3, 4, 5 -> System.out.println("Весна.");
             case 6, 7, 8 -> System.out.println("Лето");
